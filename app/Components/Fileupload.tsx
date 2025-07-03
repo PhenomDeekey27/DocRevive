e0cbce86d4dc6bdeef84c76650170325554c4630
KasthuriRangan <krangan415@gmail.com>
2025-07-03 13:26:04 +0530

    icon name fix

--- app/Components/Fileupload.tsx
+++ app/Components/Fileupload.tsx
@@ -187,7 +187,7 @@ ${textcontent}
 
       <input ref={inputRef} onChange={handleFileChange} type="file" accept='.pdf , image/png , image/jpg , image/jpeg' className='hidden' />
       <button className='bg-primary text-white p-2 flex items-center gap-4 rounded-md' onClick={() => inputRef.current?.click()}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-file-icon lucide-file"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-file-icon lucide-file"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /></svg>
 
         <span>Upload Files</span>
 
